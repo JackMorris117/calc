ea8fc8ca35489250df2e2ec01ae64e1f0c769af9
Jack Morris <morriseo94@gmail.com>
2020-08-24 16:36:23 +0100

    revision

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -15,11 +15,13 @@ describe('calculator', function () {
     calculator.add(4)
     assert.equal(5, calculator.runningTotal)
   })
+
   it('should subtract numbers', function(){
     calculator.previousTotal = 7;
     calculator.subtract(4)
     assert.equal(3, calculator.runningTotal)
   })
+  
   it('should multiply numbers', function(){
     calculator.previousTotal = 3;
     calculator.multiply(5)
@@ -30,7 +32,7 @@ describe('calculator', function () {
     calculator.divide(7)
     assert.equal(3, calculator.runningTotal)
   })
-  it('should respond to user clicks', function () {
+  it('should respond to user sybol clicks', function () {
     calculator.operatorClick('+');
     assert.strictEqual('+', calculator.previousOperator);
     calculator.operatorClick('-');
@@ -39,6 +41,18 @@ describe('calculator', function () {
     assert.strictEqual('*', calculator.previousOperator);
     calculator.operatorClick('/');
     assert.strictEqual('/', calculator.previousOperator);
+  })
+  it('should respond to multiple user operations/number clicks including the clear click', function () {
+    calculator.previousTotal = 3;
+    calculator.add(4)
+    calculator.previousTotal = 7;
+    calculator.clearClick()
+    calculator.subtract(5)
+    assert.equal(2, calculator.runningTotal)
+    calculator.clearClick()
+    assert.equal(0, calculator.runningTotal)
+
+    
   })
   
 });
